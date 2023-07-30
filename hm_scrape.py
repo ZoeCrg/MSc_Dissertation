809cd22e8a2fbb52ec531389ebd649e78c34a6b6
ZoeCrg <115108738+ZoeCrg@users.noreply.github.com>
2023-07-30 18:47:22 +0100

    Update hm_scrape.py head5

--- hm_scrape.py
+++ hm_scrape.py
@@ -160,6 +160,9 @@ while offset < total_products:
     new_df = pd.DataFrame(data['products'])
     
     new_df['fabric'] = new_df.link.apply(fabric_function)
+
+#remove
+    new_df = new_df.head(5)
     
     df = pd.concat([df, new_df], axis=0, ignore_index=True)
     offset += page_size
@@ -171,12 +174,178 @@ while offset < total_products:
 # for line in csv_io:
 #     print(line.strip())
 
+
+
+def read_the_csv(file):
+    df = pd.read_csv(file)
+    return df
+
+def parse_material_compositions(teststring, recycled_tag='Recycled'):
+    try:
+        splitup = (teststring.split(","))
+    except:
+        return [[],[],[]]
+    lists = []
+    newlist = []
+    for i in splitup:
+        i = i.strip()
+        if len(i) == 0:
+            continue
+        if i[0] == "[":
+            try:
+                if newlist:
+                    lists.append(newlist)
+            except: 
+                pass
+            newlist = []
+        newlist.append(i)
+    lists.append(newlist)
+    for l in lists:
+        try:
+            b = l[0].split('] ')
+            l[0] = b[1]
+        
+            if b[0] == "[Recycled":  
+                l.insert(0, "Recycled")
+        except: 
+            pass
+    return lists
+
+def get_recycled(lists):
+    for i in lists:
+        try:
+            if(i[0] == "Recycled"):
+                return i[1:]
+        except:
+            pass
+
+def get_main(lists):
+    return lists[0]
+
+def get_secondary(lists):
+    if len(lists) <2:
+        return
+    secondary = []
+    for i in lists[1:]:
+        if(i[0] == "Recycled"):
+            continue
+        secondary.append(i)
+    if len(secondary) == 0:
+        return
+    return secondary
+
+import re
+
+def remove_trademark_symbol(input_string):
+    # Define a regular expression pattern to match the trademark symbol (\u2122 and \U00002122) in a case-insensitive manner
+    pattern = r'\\[Uu]([0-9a-fA-F]{4,8})'
+
+    # Use re.sub() to replace all occurrences of the pattern with an empty string
+    cleaned_string = re.sub(pattern, '', input_string)
+
+    return cleaned_string
+
+def split_material_and_percentage(input_string):
+    # Define a regular expression pattern to match the material and percentage parts
+    pattern = r'^(.*?)(\d+\%)$'
+    
+    # Use the re.match function to find the pattern in the input_string
+    match = re.match(pattern, input_string)
+    
+    if match:
+        # The first group (index 1) contains the material part
+        material = match.group(1).strip()
+        
+        # The second group (index 2) contains the percentage part
+        percentage = match.group(2).strip()
+        material = remove_trademark_symbol(material)
+        return material, percentage
+    else:
+        # If no match is found, return the entire string as material and an empty string for percentage
+        return input_string.strip(), ""
+
+def add_columns_and_values(df):
+    # Iterate through each row of the DataFrame
+    for index, row in df.iterrows():
+        composition_list = get_main((parse_material_compositions(row["fabric"])))
+        print(composition_list)
+        # Extracting the material name and percentage from the list
+        for item in composition_list:
+            try:
+                material, percentage = split_material_and_percentage(item)
+                percentage = float(percentage.strip('%'))
+                # Adding a new column to the DataFrame if it doesn't exist
+                if material.upper() not in df.columns:
+                    df[material.upper()] = 0  # Initializing the column with None
+                # Assigning the percentage value to the corresponding cell
+                df.at[index, material.upper()] = percentage
+            except:\')
+                link = str(row['link'])
+                new_fabric_value = fabric_function(link)
+                df.at[index, 'fabric'] = new_fabric_value 
+                composition_list = get_main((parse_material_compositions(new_fabric_value)))
+                # Extracting the material name and percentage from the list
+                for item in composition_list:
+                    try:
+                        material, percentage = split_material_and_percentage(item)
+                        percentage = float(percentage.strip('%'))
+                        # Adding a new column to the DataFrame if it doesn't exist
+                        if material.upper() not in df.columns:
+                            df[material.upper()] = 0  # Initializing the column with None
+                        # Assigning the percentage value to the corresponding cell
+                        df.at[index, material.upper()] = percentage
+                    except:
+                        print('failed again')
+                        pass
+                
+                
+                pass
+            
+    
+    return df
+
+
+def add_recycled_columns_and_values(df):
+    for index, row in df.iterrows():
+        composition_list = get_recycled((parse_material_compositions(row["fabric"])))
+        if composition_list:
+            for item in composition_list:
+                try:
+                    
+                    material, percentage = split_material_and_percentage(item)
+                    
+                    material = 'Sustainable ' + material
+                    percentage = float(percentage.strip('%'))
+                    # Adding a new column to the DataFrame if it doesn't exist
+                    if material.upper() not in df.columns:
+                        df[material.upper()] = 0  # Initializing the column with None
+                    # Assigning the percentage value to the corresponding cell
+                    df.at[index, material.upper()] = percentage
+                except:
+                    pass
+    
+    return df
+import os
+def process_build_file(df, dt):
+    materialsdf =add_columns_and_values(df)
+    recycledmaterialsdf =add_recycled_columns_and_values(materialsdf)
+    recycledmaterialsdf['Date'] = dt
+    return recycledmaterialsdf;
+
+
+
+
+
+
+
+
 import json
 def dataframe_to_json(df):
     json_string = df.to_json(orient='records')
     return json_string
 
 current_datetime = datetime.now().strftime('%Y%m%d-%H%M%S')
+df = process_build_file(df, current_datetime)
 csv_filename = f"data/data_{current_datetime}.csv"
 
 # Save the DataFrame to the CSV file
