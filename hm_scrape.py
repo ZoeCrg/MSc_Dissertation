3a29d4f790301b3a11916085d633d03199907bc4
ZoeCrg <115108738+ZoeCrg@users.noreply.github.com>
2023-07-30 18:56:33 +0100

    Update hm_scrape.py

--- hm_scrape.py
+++ hm_scrape.py
@@ -159,10 +159,11 @@ while offset < total_products:
     data = req.json()
     new_df = pd.DataFrame(data['products'])
     
-    new_df['fabric'] = new_df.link.apply(fabric_function)
-
 #remove
     new_df = new_df.head(5)
+
+    
+    new_df['fabric'] = new_df.link.apply(fabric_function)
     
     df = pd.concat([df, new_df], axis=0, ignore_index=True)
     offset += page_size
